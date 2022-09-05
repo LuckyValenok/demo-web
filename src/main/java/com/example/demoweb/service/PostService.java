e017de4f6714fcd0464c5f44cdeed249b989c209
Vlad <luckyvalenok1@gmail.com>
2022-09-05 10:13:15 +0500

    Добавлена возможность создания постов

--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -3,13 +3,19 @@ package com.example.demoweb.service;
 import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
+    private final List<Post> posts = new ArrayList<>();
+    
     public List<Post> listAllPosts() {
-        return Arrays.asList(new Post("1", new Date()), new Post("2", new Date()), new Post("3", new Date()));
+        return posts;
+    }
+    
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
     }
 }
