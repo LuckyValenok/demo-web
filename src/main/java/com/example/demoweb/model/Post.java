26e57f1be9f616c23dd5889a47ade16b4d5a6eda
Vlad <luckyvalenok1@gmail.com>
2022-09-05 09:44:20 +0500

    Bootstrap вёрстка

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -1,15 +1,23 @@
 package com.example.demoweb.model;
 
+import java.util.Date;
+
 public class Post {
     private final String text;
+    private final Date creationDate;
     private int likes;
     
-    public Post(String text) {
+    public Post(String text, Date creationDate) {
         this.text = text;
+        this.creationDate = creationDate;
     }
     
     public String getText() {
         return text;
+    }
+    
+    public Date getCreationDate() {
+        return creationDate;
     }
     
     public int getLikes() {
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -4,11 +4,12 @@ import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
     public List<Post> listAllPosts() {
-        return Arrays.asList(new Post("1"), new Post("2"), new Post("3"));
+        return Arrays.asList(new Post("1", new Date()), new Post("2", new Date()), new Post("3", new Date()));
     }
 }
